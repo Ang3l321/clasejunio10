11721fea7f6ed9053f6baf508d319354d334d55d
Ang3l321 <168379428+Ang3l321@users.noreply.github.com>
2025-06-10 21:27:55 -0500

    Add files via upload
    blal afdw

---
+++ Script.js
@@ -0,0 +1,9 @@
+let elemento = document.createElement('li');
+elemento.textContent = 'Nuevo elemento';
+
+document.getElementById('lista').appendChild(elemento);
+
+let elemento = document.createElement('li');
+elemento.textContent = 'Nuevo elemento 2';
+
+document.getElementById('lista').appendChild(elemento);
---
+++ python101/101.py
@@ -0,0 +1,13 @@
+#Comentario
+
+"Cadena de caracteres"
+"""_summary_Es
+    estajhaidh sjkdh
+    """
+print("Yo no me llamo javier")
+print('''Esta es 
+      una impresion
+      con saltos de linea''')
+print('linea 1 \n linea 2 \n linea 3')
+print('Esta es una nueva impresión.', end=' ')
+print('Esta es otra impresión.')
